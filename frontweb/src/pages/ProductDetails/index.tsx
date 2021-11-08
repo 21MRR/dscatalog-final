6c17ebe7882e5227b73d72d8040d3f65b2a8a419
Mauro Rezende <maurorezende2012@gmail.com>
2021-11-08 19:20:19 -0300

    useEstate, useEffect and useParams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,35 @@
 import { ReactComponent as ArrowIcom } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
+type UrlParams = {
+  productId : string;
+}
+
 export const ProducDetails = () => {
+
+  const { productId } = useParams<UrlParams>();
+
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+
+    axios.get(`${BASE_URL}/products/${productId}`)
+    .then(response =>{
+     setProduct(response.data);
+
+    });
+  }, [productId]);
+
+
+
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -18,21 +43,20 @@ export const ProducDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome do Produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={2456.0} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price}/>}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do Produto</h2>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Ducimus, harum?
+                {product?.description}
               </p>
             </div>
           </div>
